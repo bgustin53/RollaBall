93f2879de075dac4e057bd7d765d2f8eab1ffb99
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2022-12-11 14:48:24 -0800

    Added Player Input Action

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*****************************************
+ * This script will control both the
+ * player actions and the UI
+ * 
+ * Author: Bruce Gustin
+ * December 11, 2022
+ ****************************************/
+
+public class PlayerController : MonoBehaviour
+{
+    // PlayerController properties
+    private Rigidbody rb;               // Allows for use of physics
+    private float movementX;            // Holds forwars/backward component of movement vector
+    private float movementY;            // Holds left/right component of movement vector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Gets keypresses from Input Actions and applies it to the Player Movement
+
+
+    // Displays pickup count and end game message to the screen
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Detects collisions with pickups and counts them
+}
