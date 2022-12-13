d009acdf1ce05d5e61957bd647a818c402ba4e27
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2022-12-13 07:59:44 -0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /*****************************************
  * This script will control both the
@@ -17,21 +18,32 @@ public class PlayerController : MonoBehaviour
     private float movementX;            // Holds forwars/backward component of movement vector
     private float movementY;            // Holds left/right component of movement vector
 
+    private float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Gets keypresses from Input Actions and applies it to the Player Movement
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 
 
     // Displays pickup count and end game message to the screen
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
         
     }
 
